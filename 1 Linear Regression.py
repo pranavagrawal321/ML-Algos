3432e07bb428b80625a80910ab1ce35caf75b325
Pranav Agrawal <pranavagrawal321@gmail.com>
2023-10-06 01:17:13 +0530

    Linear Regression

---
+++ 1 Linear Regression.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+class LinearRegression:
+    def __init__(self, lr=0.01, n_iters=1000):
+        self.lr = lr
+        self.n_iters = n_iters
+        self.weights = None
+        self.bias = None
+        
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+        
+        for i in range(self.n_iters):
+            y_predict = np.dot(X, self.weights) + self.bias
+            
+            dw = (1/n_samples) * np.dot(X.T, (y_predict - y))
+            db = (1/n_samples) * np.sum(y_predict - y)
+            
+            self.weights -= self.lr * dw
+            self.bias -= self.lr * db
+    
+    def predict(self, X):
+        return np.dot(X, self.weights) + self.bias
